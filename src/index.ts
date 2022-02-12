f6b4fdd801c4ba697bb0d04ff0ba0b10d98f04a1
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-12 19:19:11 +0000

    Ran prettier

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ const configFiles: any = {};
 const configFolderPath = path.resolve(__dirname, "config");
 
 async () => {
-	const files = await readdir(configFolderPath).catch(console.log);
+  const files = await readdir(configFolderPath).catch(console.log);
 
-	for (let i of files) {
-		const tsconfigName = i.split(".")[1];
-		configFiles[tsconfigName] = path.join(configFolderPath, i);
-	}
+  for (let i of files) {
+    const tsconfigName = i.split(".")[1];
+    configFiles[tsconfigName] = path.join(configFolderPath, i);
+  }
 };
