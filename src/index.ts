e5b476e8f5659ad04f7033aa8cc40a63a1727d5e
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-12 19:18:08 +0000

    Started code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const inquirer = require("inquirer");
+const path = require("path");
+const { writeFile, readdir, readFile } = require("fs").promises;
+
+const configFiles: any = {};
+const configFolderPath = path.resolve(__dirname, "config");
+
+async () => {
+	const files = await readdir(configFolderPath).catch(console.log);
+
+	for (let i of files) {
+		const tsconfigName = i.split(".")[1];
+		configFiles[tsconfigName] = path.join(configFolderPath, i);
+	}
+};
